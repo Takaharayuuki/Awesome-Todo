49200d6cce8725dc3bac04846af159b05eb30888
Takaharayuuki <takahara.yuuki22@gmail.com>
2020-11-22 13:40:42 +0900

    add: :style

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,27 @@
 <template>
   <q-page class="q-gutter-lg">
-    <input v-model="message" @keyup="handleKeyUp" />
+  <button style="position: absolute">
+    {{ counter }}
+  </button>
+
+    <input v-model="message"
+    @keyup.esc="clearText"
+    @mouseenter="alertMessage"
+    v-autoforcus
+    :style="errorStyle"/>
     <button @click="clearText()">clear</button>
-    <h5>{{ message }}</h5>
+
+    <div>{{ message.length }}</div>
+
+    <h5
+      v-if="message.length"
+      class="border-grey" >{{ message }}</h5>
+    <h6 v-else>No Message entered</h6>
+
+    <hr>
+
+    <p>Uppercase message: {{ messageUppercase}}</p>
+    <p>Lowercase message: {{ message | messageLowercase}}</p>
   </q-page>
 </template>
 
@@ -10,7 +29,21 @@
 export default {
   data() {
     return {
-      message: 'I Love vue.js'
+      message: 'I Love vue.js',
+      counter: 0
+    }
+  },
+  computed: {
+     messageUppercase() {
+      return this.message.toUpperCase()
+    },
+    errorStyle() {
+      if (this.message.length > 22) {
+        return {
+          'color' : 'red',
+          'background' : 'pink'
+        }
+      }
     }
   },
   methods: {
@@ -19,7 +52,39 @@ export default {
     },
     handleKeyUp(e) {
       console.log(e);
+      if(e.keyCode === 8) {
+        this.clearText()
+      } else if(e.keyCode === 13) {
+        this.alertMessage()
+      }
+    },
+    alertMessage() {
+      alert(this.message)
+    },
+  },
+  filters: {
+    messageLowercase(value) {
+      return value.toLowerCase()
+    }
+  },
+  directives: {
+    autoforcus: {
+      inserted(el) {
+        console.log('input inserted');
+        el.focus();
+      }
     }
   }
 }
 </script>
+
+<style>
+  .border-grey {
+    border: 1px solid grey;
+  }
+
+  .error {
+    color: red;
+    background: pink;
+  }
+</style>
