df4c426d073df36907e7faa117206748e652bcd2
Takaharayuuki <takahara.yuuki22@gmail.com>
2020-11-22 11:30:07 +0900

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="q-gutter-lg">
+    <input v-model="message" @keyup="handleKeyUp" />
+    <button @click="clearText()">clear</button>
+    <h5>{{ message }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'I Love vue.js'
+    }
+  },
+  methods: {
+    clearText() {
+      this.message = ""
+    },
+    handleKeyUp(e) {
+      console.log(e);
+    }
+  }
+}
+</script>
