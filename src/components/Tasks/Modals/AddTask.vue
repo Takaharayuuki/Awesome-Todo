607fb5ebaa5d0caad61da7648838eeeb86ee947d
Takaharayuuki <takahara.yuuki22@gmail.com>
2020-11-25 08:36:16 +0900

    add: date,time picker

--- src/components/Tasks/Modals/AddTask.vue
+++ src/components/Tasks/Modals/AddTask.vue
@@ -3,27 +3,67 @@
     <q-card-section class="row">
       <div class="text-h6">Add Task</div>
       <q-space />
-      <q-btn 
-      v-close-popup 
-      flat
-      round 
-      dense 
-      icon="close" />
+      <q-btn v-close-popup flat round dense icon="close" />
     </q-card-section>
 
-    <q-card-section >
-     <q-input
-     ooutlined
-     v-model="text"
-     label="Outlined"/>
+    <q-card-section>
+
+      <!-- インプット フィールド -->
+      <div class="row q-mb-sm">
+        <q-input
+          class="col"
+          outlined
+          v-model="taskToSubmit.name"
+          label="Task name" />
+      </div>
+
+      <!-- データピッカー -->
+      <div class="row q-mb-sm">
+        <q-input
+          label="Due date"
+          outlined
+          v-model="taskToSubmit.dueDate">
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy
+                ref="qDateProxy"
+                transition-show="scale"
+                transition-hide="scale">
+                <q-date v-model="taskToSubmit.dueDate">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-date>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+
+      <!-- タイムピッカー -->
+      <div class="row q-mb-sm">
+        <q-input
+          label="Due time"
+          outlined
+          v-model="taskToSubmit.dueTime" >
+          <template v-slot:append>
+            <q-icon name="access_time" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-time v-model="taskToSubmit.dueTime">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-time>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+
     </q-card-section>
 
     <q-card-actions align="right">
-      <q-btn 
-        flat 
-        label="Save" 
-        color="primary" 
-        v-close-popup />
+      <q-btn flat label="Save" color="primary" v-close-popup />
     </q-card-actions>
   </q-card>
 </template>
@@ -32,13 +72,13 @@
 export default {
   data() {
     return {
-      taskToSubmit : {
-        name: '',
-        dueDate: '',
-        dueTime: '',
-        completed: false
-      }
-    }
-  }
+      taskToSubmit: {
+        name: "",
+        dueDate: "",
+        dueTime: "",
+        completed: false,
+      },
+    };
+  },
 };
 </script>
