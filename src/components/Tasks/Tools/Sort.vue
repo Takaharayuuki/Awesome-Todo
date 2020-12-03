a74c235fd47e09d009456818d916d200c3dbcc80
Takaharayuuki <takahara.yuuki22@gmail.com>
2020-12-04 08:30:54 +0900

    add: store to setSort

--- src/components/Tasks/Tools/Sort.vue
+++ src/components/Tasks/Tools/Sort.vue
@@ -1,29 +1,53 @@
 <template>
   <q-select
     filled
-    v-model="model"
+    v-model="sortBy"
     class="col q-ml-sm"
     :options="options"
+    emit-value
+    map-options
     label="Sort by"
     stack-label
   />
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
   data() {
     return {
-      model: null,
-
-      options: ["Google", "Facebook", "Twitter", "Apple", "Oracle"],
-
+      options: [
+        {
+          label: 'Name',
+          value: 'name'
+        },
+        {
+          label: 'Date',
+          value: 'dueDate'
+        }
+      ]
     };
+  },
+  computed: {
+    ...mapState('tasks', ['sort']),
+    sortBy: {
+      get() {
+        return this.sort
+      },
+      set(value) {
+        this.setSort(value)
+      }
+    }
+  },
+  methods: {
+    ...mapActions('tasks', ['setSort'])
   },
 };
 </script>
 
 <style scoped>
-  .q-select {
-    flex: 0 0 112px;
-  }
+.q-select {
+  flex: 0 0 112px;
+}
 </style>
