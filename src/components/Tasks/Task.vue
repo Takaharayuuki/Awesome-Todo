a2f201a76145b7e43b9a883d1419c52e2fce9dec
Takaharayuuki <yuuki.barber@gmail.com>
2020-12-04 19:10:48 +0900

    add: 検索で一致するタスクを強調表示する

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -1,19 +1,16 @@
 <template>
   <q-item
-    @click="updateTask({ id: id, updates: { completed: !task.completed }})"
+    @click="updateTask({ id: id, updates: { completed: !task.completed } })"
     :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
     clickable
     v-touch-hold:1000.mouse="showEditTaskModal"
     v-ripple
   >
     <q-item-section side top>
-      <q-checkbox v-model="task.completed"
-      class="no-pointer-events"/>
+      <q-checkbox v-model="task.completed" class="no-pointer-events" />
     </q-item-section>
     <q-item-section>
-      <q-item-label :class="{ 'text-strikethrough': task.completed }"
-        >{{ task.name }}</q-item-label
-      >
+      <q-item-label :class="{ 'text-strikethrough': task.completed }" v-html="$options.filters.searchHighlight(task.name, search)"></q-item-label>
     </q-item-section>
 
     <q-item-section v-if="task.dueDate" side top>
@@ -22,10 +19,14 @@
           <q-icon name="event" size="18px" class="q-mr-xs" />
         </div>
         <div class="column">
-          <q-item-label caption class="row justify-end">
-            {{ task.dueDate }}
+          <q-item-label
+            caption
+            class="row justify-end">
+            {{ task.dueDate | dateFormat }}
           </q-item-label>
-          <q-item-label caption class="row justify-end">
+          <q-item-label
+            caption
+            class="row justify-end">
             <small>
               {{ task.dueTime }}
             </small>
@@ -42,61 +43,84 @@
           round
           dense
           color="primary"
-          icon="edit" />
-      <q-btn
+          icon="edit"
+        />
+        <q-btn
           @click.stop="promptToDelete(id)"
           flat
           round
           dense
           color="red"
-          icon="delete" />
+          icon="delete"
+        />
       </div>
     </q-item-section>
 
     <q-dialog v-model="showEditTask">
-      <edit-task  @close="showEditTask = false"
-      :task="task"
-      :id="id"/>
+      <edit-task @close="showEditTask = false" :task="task" :id="id" />
     </q-dialog>
-
   </q-item>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
+import { date } from "quasar";
 
 export default {
-  name: 'Task',
-  props: ['task', 'id'],
+  name: "Task",
+  props: ["task", "id"],
   data() {
     return {
-      showEditTask: false
-    }
+      showEditTask: false,
+    };
+  },
+  computed: {
+    ...mapState("tasks", ["search"]),
   },
   methods: {
-    ...mapActions('tasks', ['updateTask','deleteTask']),
+    ...mapActions("tasks", ["updateTask", "deleteTask"]),
     showEditTaskModal() {
-      this.showEditTask = true
+      this.showEditTask = true;
     },
     promptToDelete(id) {
       // 削除確認ダイアログ
-      this.$q.dialog({
-        title: 'confilm',
-        message: 'Realy Delete?',
-        ok: {
-          push: true
-        },
-        cancel: {
-          color: 'negative'
-        },
-        persistent: true
-      }).onOk(() => {
-        this.deleteTask(id)
-      })
+      this.$q
+        .dialog({
+          title: "confilm",
+          message: "Realy Delete?",
+          ok: {
+            push: true,
+          },
+          cancel: {
+            color: "negative",
+          },
+          persistent: true,
+        })
+        .onOk(() => {
+          this.deleteTask(id);
+        });
     },
   },
-  components: { "edit-task": require("components/Tasks/Modals/EditTask").default, },
-}
+  filters: {
+    dateFormat(value) {
+      return date.formatDate(value, "YYYY年MM月DD日");
+    },
+    searchHighlight(value, search) {
+      console.log(value);
+      console.log(search);
+      if (search) {
+        let searchRegExp = new RegExp(search, 'i')
+        return value.replace(searchRegExp, (match) => {
+          return '<span class="bg-yellow-6">' + match + '</span>'
+        });
+      }
+      return value;
+    },
+  },
+  components: {
+    "edit-task": require("components/Tasks/Modals/EditTask").default,
+  },
+};
 </script>
 
 <style>
